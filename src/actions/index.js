01d94d7508f84508a636128b6d4446eb7cf908d2
Gabriel <gabrielavila04@protonmail.com>
2021-01-25 21:05:23 -0300

    update actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,11 +3,11 @@ import _ from "lodash"
 
 export const fetchCategoriesAndGames = () => async(dispatch, getState) => {
   await dispatch(fetchCategories());
-
-  const test = _.chain(getState().categories)
-  .forEach(({id}) => {console.log(id)} )
+  _.chain(getState().categories)
+  .map('id')
+  .uniq()
+  .forEach((id) => {dispatch(fetchGames(id))} )
   .value()
-  console.log(test, "test")
 }
 
 export const fetchCategories = () => async (dispatch) => {
@@ -15,8 +15,9 @@ export const fetchCategories = () => async (dispatch) => {
   dispatch({ type: "FETCH_CATEGORIES", payload: response.data });
 };
 
-export const fetchGames = () => async (dispatch) => {
+export const fetchGames = (id) => async (dispatch) => {
   const response = await info.get('games.json');
-  dispatch({type: "FETCH_GAMES", payload: response.data})
+  const game = response.data.filter((game) => game.catId === id) 
+  dispatch({type: "FETCH_GAMES", payload: game})
 }
 
