7916ec40345d8d86dbbf75cfc3deb911950c6db5
Gabriel <gabrielavila04@protonmail.com>
2021-02-03 10:51:30 -0300

    update components

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,27 +1,63 @@
 import React from "react";
 import { connect } from "react-redux";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.css";
 
 import Game from "./Game";
 import { fetchCategoriesAndGames } from "../actions";
+import "../css/main.min.css";
 
 class Categories extends React.Component {
-  componentDidMount(){
-    this.props.fetchCategoriesAndGames()
+  componentDidMount() {
+    this.props.fetchCategoriesAndGames();
   }
-    render() {
-    console.log(this.props)
+  games() {
+    return this.props.listGames.map((game) => game.map(gm=>{
+      console.log(gm)
+      return <SwiperSlide tag="li" key={gm.id} ><Game gameProps={gm}  /></SwiperSlide>
+    }));
+  }
+  render() {
     const name = this.props.category.name;
-    return (
+    
+    return this.props.listGames.length === 0 ? null : (
       <div className="category">
-        <div className="category-name">{name}</div>
-        {}
+        <h2 className="category-name">{name}</h2>
+        <React.Fragment>
+          <Swiper
+            tag="section"
+            wrapperTag="ul"
+            id="main"
+            spaceBetween={10}
+            slidesPerView={1}
+            breakpoints={{
+              // when window width is >= 768px
+              768: {
+                spaceBetween: 10,
+                slidesPerView: 2,
+              },
+              300: {
+                spaceBetween: 20,
+                slidesPerView: 1.3,
+              },
+            }}
+          >
+            {this.games()}
+          </Swiper>
+        </React.Fragment>
       </div>
     );
   }
 }
 
 const mapStateToProps = (state, ownProperty) => {
-  return { listGames: state.games.find(game => {return ownProperty.category.id === game[0].catId}), dummy: "ooo" };
+  var listGames = state.games.filter((game) => {
+    var resp = game[0].catId !== ownProperty.category.id;
+    return resp;
+  });
+  return { listGames };
 };
 
-export default connect(mapStateToProps, { fetchCategoriesAndGames })(Categories);
+export default connect(mapStateToProps, { fetchCategoriesAndGames })(
+  Categories
+);
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,12 +1,21 @@
 import React from "react";
-import {connect} from "react-redux";
+import { connect } from "react-redux";
 
-const Game = () => {
-  return(
-    <div>Gameee NAMEEE</div>
-  )
+class Game extends React.Component {
+  render() {
+    const game = this.props.gameProps;
+    return (
+      <div className="game">
+        <h3 className="game-name">{game.name}</h3>
+        <div className="game-description">{game.desc}</div>
+        <div className="game-button"> <button>Play</button> </div>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = (state, ownProperty) => {
-  return {game: state}
-}
+  return { game: state };
+};
+
+export default connect(mapStateToProps)(Game);
